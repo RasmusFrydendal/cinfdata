a8ff7b70e0949d9ae501b3b9f4550c489b1c2b7d
Kenneth Nielsen <k.nielsen81@gmail.com>
2017-03-02 21:29:32 +0100

    Ported xyplot.php except for tof part. Also port link.php, but could not complete off site

--- links/link.php
+++ links/link.php
@@ -1,7 +1,7 @@
 <?php
 
 include("../common_functions_v2.php");
-$db = std_db();
+$dbi = std_dbi();
 
 $URL = $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 $REFERER = $_SERVER["HTTP_REFERER"];
@@ -11,15 +11,15 @@ echo(html_header());
 
 if($_GET["url"] == "checked") {
   $query = "INSERT INTO short_links (url,comment) VALUES (\"" . $REFERER . "\",\"" . $comment . "\")";
-  $result  = mysql_query($query, $db);
+  $result = mysqli_query($db, $query);
   $query = "SELECT max(id) from short_links";
-  $max_id = single_sql_value($db, $query, 0);
+  $max_id = single_sql_value($dbi, $query, 0);
   echo("<br><b>Your full URL:</b> " . $REFERER . " <br> <b>Can now be reached at:</b> <a href=\"http://www.cinfdata.fysik.dtu.dk/links/link.php?id=" . $max_id . "\">http://www.cinfdata.fysik.dtu.dk/links/link.php?id=" . $max_id . "</a><br>");
   echo("<input type=\"button\" value=\"Back to Previous Page\" onClick=\"javascript: history.go(-1)\">");
 } else {
   $id = $_GET["id"];
   $query = "SELECT url from short_links WHERE id=" . $id . "";
-  $url = single_sql_value($db, $query, 0);
+  $url = single_sql_value($dbi, $query, 0);
   header("Location: $url");
   exit;
 }
